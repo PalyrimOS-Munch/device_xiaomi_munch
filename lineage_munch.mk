b41aba15d5cb87830654d33a665287ba1e1c24a7
VINC3NT <88191487+Vincent4440@users.noreply.github.com>
2023-02-24 09:48:05 +0530

    munch: PalyrimOS Bringup

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -8,16 +8,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
+# Inherit some common PalyrimOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
-# Include GMS by default, but rely on environment variable just in case we don't want to build with GMS conditionally
-ifneq ($(NO_GMS),true)
-$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
-endif
+# PalyrimOS flags
+WITH_GMS := true
+PALYRIM_MAINTAINER := vinc3nt
 
 TARGET_SUPPORTS_CALL_RECORDING := true
 
